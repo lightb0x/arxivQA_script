3abb4e06513e24221800472797c7b4e617709907
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2023-11-16 14:23:58 +0900

    [misc] check legacy structure

--- utils/data_reconstruct.py
+++ utils/data_reconstruct.py
@@ -7,8 +7,8 @@ PATH_PARENT = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 DATA_DIR = os.path.join(PATH_PARENT, "ar5iv")
 
 if __name__ == "__main__":
-    print(os.listdir(DATA_DIR))
     for id in os.listdir(DATA_DIR):
+        assert len(id.split(".")) == 2
         yymm, num = id.split(".")
         if not os.path.exists(os.path.join(DATA_DIR, yymm)):
             print(yymm)
