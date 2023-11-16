3c9a4a5f3577ad0966ca3d8ac1a26cb9ec037341
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2023-11-16 17:13:50 +0900

    [feat] specify clean and dirty

--- data_clean.py
+++ data_clean.py
@@ -1,5 +1,6 @@
 # remove following:
 # * failed to render on ar5iv
+# * failed to convert to markdown
 # * duplicated (papers with versions)
 import os
 import json
@@ -8,12 +9,14 @@ PATH_PARENT = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 DATA_DIR = os.path.join(PATH_PARENT, "ar5iv")
 
 IN_FILENAME = "paper_id_num_token.json"
-OUT_FILENAME = "clean_dset.json"
+CLEAN = "clean_dset.json"  # dedup id --> actual id
+DIRTY = "dirty_dset.json"  # actual id --> reason
 
 if __name__ == "__main__":
     skipped = 0
     total_num_tok = 0
     clean = {}
+    dirty = {}
 
     num_tokens = {}
     with open(IN_FILENAME, "r") as f:
@@ -24,12 +27,24 @@ if __name__ == "__main__":
         num = num.split("v")[0]
         dedup_id = ".".join([ym, num])
 
-        if num_tok < 256 or num in clean:
-            # skip ar5iv failure and duplicates
+        if num_tok < 256:
+            # skip ar5iv failure
             skipped += 1
+            dirty[id] = "ar5iv/pandoc"
+        elif dedup_id in clean:
+            # skip duplicates
+            skipped += 1
+            dirty[id] = "duplicate"
         else:
             clean[dedup_id] = id  # actual link
             total_num_tok += num_tok
 
     print(f"skipped {skipped} out of {len(num_tokens)} papers")
     print(f"total {total_num_tok} tokens")
+
+    print("saving...")
+    with open(CLEAN, "w") as f:
+        json.dump(clean, f, indent=4)
+    with open(DIRTY, "w") as f:
+        json.dump(dirty, f, indent=4)
+    print("done!")
